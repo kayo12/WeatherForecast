a27ecccec43c2f3f139e286c7ad5e2946e9dd5d3
Kayo campos <kayonbahenrique@gmail.com>
2022-07-21 01:10:57 -0300

    feat: inicio home page

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const isMobile: boolean = true;
+</script>
+
+<template>
+  <div id="main" class="home">
+    <div class="info-home">
+      <div class="div2"></div>
+      <div class="div3"></div>
+    </div>
+    <div class="form">Formulario</div>
+    <Clock />
+  </div>
+</template>
+
+<style scoped lang="stylus">
+  #main{
+    background: red;
+    .info-home
+      background: blue;
+      .div2
+        background: black
+      .div3
+        background: purple
+      if(isMobile)
+        background: green
+  }
+
+  .info-home
+    color red
+      .info-home2{
+        color blue
+      }
+
+  .info-home{
+    padding: 2rem;
+  }
+</style>
